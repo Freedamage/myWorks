78aec2b54f42a1ed54a9ea81bd09248491a60b47
Freedamage <tema4200@gmail.com>
2017-01-02 23:32:55 +0700

    Create gocasino.js

---
+++ JS/gocasino.js
@@ -0,0 +1,51 @@
+var start = 2; 
+
+for (i = 1; i <= 250; i++){
+var seconds = window.document.getElementsByClassName("rolling")[0].getElementsByTagName("span")[0].innerHTML;
+var seconds = seconds.replace('Rolling in ',''); 
+var seconds = parseInt(seconds);                 
+if (seconds <= 2 && seconds >= 0.1){
+      i = 2;
+      var red = window.document.getElementsByClassName("col-xs-6 red-total total-bet-amount")[0].innerHTML;
+      var black = window.document.getElementsByClassName("col-xs-6 black-total total-bet-amount")[0].innerHTML;
+      var green = window.document.getElementsByClassName("col-xs-6 green-total total-bet-amount")[0].innerHTML; 
+      var red = parseInt(red);
+      var black = parseInt(black);
+      var green = parseInt(green);
+      
+      var sum = window.document.getElementsByClassName("value")[0].innerHTML;
+       
+        if (red > black){
+        try {
+           window.document.getElementsByClassName("bet")[0].value=betOn;
+           }catch(ex){
+                      window.document.getElementsByClassName("bet")[0].value=start;
+           }
+           window.document.getElementsByClassName("black-bet-button bet-button")[0].click();
+        }
+        else if (black > red){
+        try {
+           window.document.getElementsByClassName("bet")[0].value=betOn;
+           }catch(ex){
+                      window.document.getElementsByClassName("bet")[0].value=start;
+           }
+           window.document.getElementsByClassName("red-bet-button bet-button")[0].click();              
+        } 
+           iimPlayCode("WAIT SECONDS=17");
+           var balance = window.document.getElementsByClassName("value")[0].innerHTML;
+           if (balance < sum){
+           try{
+             betOn = betOn*2;
+               }catch(ex){
+               betOn = start;
+               betOn = betOn*2;
+               }
+           }
+	    else{
+             betOn = start;
+		}
+		iimDisplay('Was money a bet ago: '+sum+'\nMoney after bet is: '+balance+'\nCurrent bet is: '+betOn);
+		
+ }
+iimPlayCode("WAIT SECONDS=1");
+}
